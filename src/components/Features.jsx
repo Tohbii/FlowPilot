7f357075d713e8767a98ca0ac0ce513bf8cfed12
Tohbii <olabodetobiloba21@gmail.com>
2026-04-03 22:21:18 +0100

    add padding to features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,7 +3,7 @@ import { Calendar, Brain, BarChart, CheckSquare } from "lucide-react";
 
 const Features = () => {
   return (
-    <div  id="features" className=' bg-gray-50 text-center max-w-full mx-autopy-10 md:py-12  scroll-mt-20'>
+    <div  id="features" className=' bg-gray-50 text-center max-w-full mx-autopy-10 py-10 md:py-12 scroll-mt-20'>
         <h2 className='text-3xl md:text-4xl font-bold text-slate-900 mb-4'>
             Everything you need to stay <span className='text-indigo-600'>productive</span>
         </h2>
